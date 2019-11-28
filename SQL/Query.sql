c938b86c1eb817f683474a9ec4d2685875f028b3
bosskunggg <b.bosskungzz@gmail.com>
2019-11-28 23:39:05 +0700

    query1&3

--- SQL/Query.sql
+++ SQL/Query.sql
@@ -1,22 +1,25 @@
 SELECT * FROM nurse ;
 
-select * from storteste ;
-select * from equipment;
-select * from drug;
+select Patient_ID , Doctor_ID , Diagnostc from diagnose natural join patient ;
+select * from Doctor join nurse ;
 
-select * from Doctor;
-select * from patient;
-select * from medicate;
 
-Delete from nurse where Nurse_ID = "000001" ;
-delete from equipment where Store_ID = '3' ;
-delete from store where Store_ID = '1' ;
-delete from drug where Store_ID = '1' ;
+/*Query1*/
+select * from prescription natural join pharmacist where prescription.Pharmacist_ID = Pharmacist_ID ;
+select  Pharmacist_Name , Pharmacist_Family_Name ,count(Pharmacist_ID) from prescription natural join pharmacist group by Pharmacist_ID having count(Pharmacist_ID) > 2;
+/*Query2*/
+select * from diagnose ;
 
-delete from doctor where Doctor_ID = '000001' ;
+/*Query3*/
+create view medprice as
+select patient_ID ,bill_id, medprocedure_price from Bill 
+		natural join check_bill natural join listofprocedure_id natural join medprocedure   ; 
+create view prescription_price as
+select Patient_ID , Bill_ID , Store_ID , (Price*Quantities) as pre_price from bill 
+		natural join check_bill natural join prescription natural join prescription_item natural join store ;
+create view prescription_price_withgroupBy as
+	select patient_id , bill_id , sum(pre_price) as prescription_price from prescription_price group by pre_price;
+select * from medprice ;
+select patient_id , (medprocedure_price + prescription_price ) as total_price from medprice natural join prescription_price_withgroupby ;
 
-update store 
-set Store_ID = '99'
-where Store_ID = '1' ;
-
-select * from appointment ;
+drop view prescription_price_withgroupBy ;
