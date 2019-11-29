97039ab4a0deada112b646c57796ad9f17c2ea7e
bosskunggg <b.bosskungzz@gmail.com>
2019-11-29 17:06:12 +0700

    Query1&3 update

--- SQL/Query.sql
+++ SQL/Query.sql
@@ -5,21 +5,27 @@ select * from Doctor join nurse ;
 
 
 /*Query1*/
-select * from prescription natural join pharmacist where prescription.Pharmacist_ID = Pharmacist_ID ;
-select  Pharmacist_Name , Pharmacist_Family_Name ,count(Pharmacist_ID) from prescription natural join pharmacist group by Pharmacist_ID having count(Pharmacist_ID) > 2;
+select  Pharmacist_Name , Pharmacist_Family_Name ,count(Pharmacist_ID) as num_of_prescription_by_pharmacist from prescription natural join pharmacist group by Pharmacist_ID having count(Pharmacist_ID) > 3;
 /*Query2*/
 select * from diagnose ;
 
 /*Query3*/
 create view medprice as
 select patient_ID ,bill_id, medprocedure_price from Bill 
-		natural join check_bill natural join listofprocedure_id natural join medprocedure   ; 
+		natural join check_bill natural join listofprocedure_id natural join medprocedure ;
+        
 create view prescription_price as
 select Patient_ID , Bill_ID , Store_ID , (Price*Quantities) as pre_price from bill 
 		natural join check_bill natural join prescription natural join prescription_item natural join store ;
+        
 create view prescription_price_withgroupBy as
-	select patient_id , bill_id , sum(pre_price) as prescription_price from prescription_price group by pre_price;
-select * from medprice ;
-select patient_id , (medprocedure_price + prescription_price ) as total_price from medprice natural join prescription_price_withgroupby ;
+	select patient_id , sum(pre_price) as prescription_price from prescription_price group by patient_id;
+    
+create view med_price_withgroupBy as
+	select patient_id , sum(medprocedure_price) as med_price from medprice group by patient_id ;
 
-drop view prescription_price_withgroupBy ;
+create view total_pricemed_pricepre as
+	select patient_id , med_price , prescription_price from med_price_withgroupBy natural join prescription_price_withgroupBy;
+
+select patient_id , med_price, prescription_price, (med_price+prescription_price) as total_price from total_pricemed_pricepre;
+
